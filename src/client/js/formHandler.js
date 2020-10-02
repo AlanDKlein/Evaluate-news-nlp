efd844be6ca43c4e6faaedd3909aad31b10d21b6
Alan Klein <70178745+AlanDKlein@users.noreply.github.com>
2020-10-02 11:31:01 -0500

    various updates

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,28 @@
 function handleSubmit(event) {
+    console.log("in handle submit")
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
+    
     fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    .then(res => {
+        return res.json()
     })
+    .then(function(data) {
+        document.getElementById('results').innerHTML = data.message
+    })    
 }
+
+// The below is what it was from the start.
+    // fetch('http://localhost:8080/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
+    //}
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -4,25 +4,34 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const cors = require('cors')
+var bodyParser = require('body-parser')
 
 console.log(`API Key = ${process.env.API_KEY}`);
 
 const app = express()
 app.use(cors());
 app.use(express.static('dist'))
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 console.log(__dirname)
+app.use(express.static('dist'))
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8080!') 
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
